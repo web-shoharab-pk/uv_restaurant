8a0e4c9980a3683ad0878b833c11d73ded6b4baa
md shoharab pk <76746357+web-shoharab-pk@users.noreply.github.com>
2022-03-12 23:55:41 +0600

    deploy

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -1,8 +1,9 @@
+// Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyBd5FFYrMQntJ3RgVfD46y76tzIkEf3Qc4",
-    authDomain: "friends-foods.firebaseapp.com",
-    projectId: "friends-foods",
-    storageBucket: "friends-foods.appspot.com",
-    messagingSenderId: "650287961442",
-    appId: "1:650287961442:web:8c99ceaf82e62fcdfec83b"
-  };
+  apiKey: "AIzaSyDNuQ8KF8HcjUQeDp8W8x_x_l8lZDoqnWs",
+  authDomain: "friends-food-uv.firebaseapp.com",
+  projectId: "friends-food-uv",
+  storageBucket: "friends-food-uv.appspot.com",
+  messagingSenderId: "164807133889",
+  appId: "1:164807133889:web:0ceaaab2894890c6122240"
+};
